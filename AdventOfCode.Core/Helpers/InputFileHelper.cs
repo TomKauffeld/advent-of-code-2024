1c9eee2125a3f0106508f80f765cda66b404ebd8
Tom Kauffeld <tom@kauffeld-it.fr>
2024-12-01 10:49:23 +0100

    day01

---
+++ AdventOfCode.Core/Helpers/InputFileHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Core.Helpers
+{
+    public static partial class InputFileHelper
+    {
+        public static string GetInputFilePath(int day, bool test = false)
+        {
+            return Path.Combine(
+                Environment.CurrentDirectory,
+                "..",
+                "..",
+                "..",
+                "..",
+                "AdventOfCode.Data",
+                $"day{day:00}",
+                test ? "test.txt" : "input.txt"
+            );
+        }
+
+        public static FileStream GetInputFile(int day, bool test = false)
+        {
+            string path = GetInputFilePath(day, test);
+            return File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+        }
+
+        public static async Task<Tuple<List<int>, List<int>>> GetDualNumbers(int day, bool test = false)
+        {
+            List<int> firstList = [];
+            List<int> secondList = [];
+
+            await using (FileStream fileStream = GetInputFile(day, test))
+            {
+                using (StreamReader reader = new(fileStream))
+                {
+                    Regex regex = DualNumberRegex();
+                    while (await reader.ReadLineAsync() is { } line)
+                    {
+                        line = line.Trim();
+                        if (line.Length < 1)
+                            continue;
+                        Match match = regex.Match(line);
+                        if (match.Success &&
+                            int.TryParse(match.Groups["a"].Value, out int a) &&
+                            int.TryParse(match.Groups["b"].Value, out int b))
+                        {
+                            firstList.Add(a);
+                            secondList.Add(b);
+                        }
+                        else
+                        {
+                            throw new Exception($"Invalid line found: {line}");
+                        }
+
+                    }
+                }
+            }
+
+            return Tuple.Create(firstList, secondList);
+        }
+
+        [GeneratedRegex("^(?<a>[0-9]+) +(?<b>[0-9]+)$")]
+        private static partial Regex DualNumberRegex();
+    }
+}
---
+++ AdventOfCode.Day01/Program.cs
@@ -0,0 +1,57 @@
+﻿using AdventOfCode.Core.Helpers;
+
+namespace AdventOfCode.Day01
+{
+    internal static class Program
+    {
+        private static async Task Part01()
+        {
+            Tuple<List<int>, List<int>> data = await InputFileHelper.GetDualNumbers(01);
+            List<int> left = data.Item1.Order().ToList();
+            List<int> right = data.Item2.Order().ToList();
+            if (left.Count != right.Count)
+            {
+                Console.WriteLine("Invalid lengths");
+                return;
+            }
+
+            int[] results = new int[left.Count];
+            Parallel.For(0, left.Count, i =>
+            {
+                int d = Math.Abs(left[i] - right[i]);
+                results[i] = d;
+            });
+            int result = results.Sum();
+            Console.WriteLine($"Found sum: {result}");
+        }
+
+        private static async Task Part02()
+        {
+            Tuple<List<int>, List<int>> data = await InputFileHelper.GetDualNumbers(01);
+            Dictionary<int, int> multipliers = new();
+            Console.WriteLine("Creating Dictionary");
+            foreach (int item in data.Item2)
+                multipliers[item] = 1 + multipliers.GetValueOrDefault(item, 0);
+
+            Console.WriteLine("Calculating Sum");
+            int[] results = new int[data.Item1.Count];
+            Parallel.For(0, data.Item1.Count, i =>
+            {
+                int d = data.Item1[i];
+                int s = d * multipliers.GetValueOrDefault(d, 0);
+                results[i] = s;
+            });
+            int result = results.Sum();
+            Console.WriteLine($"Found sum: {result}");
+        }
+
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Part 01");
+            await Part01();
+            Console.WriteLine("Part 02");
+            await Part02();
+            Console.ReadLine();
+        }
+    }
+}
